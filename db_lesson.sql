ce81fe765b6aa4b43f63e7dbe50600e2204f87fe
八島 陸 <yassymyon@myons-MacBook-Air.local>
2024-12-23 20:21:28 +0900

    12/23 fix

--- db_lesson.sql
+++ db_lesson.sql
@@ -5,6 +5,11 @@ create table departments (
   created_at timestamp null default CURRENT_TIMESTAMP ,
   updated_at TIMESTAMP null default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+/* 12/23修正 */
+自動で値が入るカラム: department_id, created_at, updated_at
+理由: department_id= カラムの値を指定せずにレコード作成した場合、自動で連続的な数値を生成するauto_incrementが指定されているため、自動で連番となる値が入力される。
+created_at= DEFAULTで初期値を指定することができるため、値がnullの場合自動で現在の時刻であるCURRENT_TIMESTAMPが入力される。
+updated_at= DEFAULTで初期値を指定することができるため、値がnullの場合自動で現在の時刻であるCURRENT_TIMESTAMPが入力される。またON UPDATEが指定されているため、レコードが更新されるたびに現在時刻が入力される。
 
 Q2
 alter table people add department_id INT unsigned null after email;
@@ -67,6 +72,11 @@ select avg(age) as average_age from people where department_id =2;
 
 Q10
 select people.name, departments.name,reports.content from people inner join departments on people.department_id = departments.department_id inner join reports on people.person_id = reports.person_id;
+/* 12/23修正 */
+OUTER JOINでなくINNER JOINを使った理由:OUTER JOINでは結合先のテーブルのデータが全て抽出され、結合出来なかったレコードのカラムがNULLとして抽出されるため。今回は日報を提出していない人のレコード(NULL)は取得したくなかったため、INNER JOINを使用した。
 
 Q11
 select people.name from people left outer join reports on people.person_id = reports.person_id where reports.content is null;
+/* 12/23修正 */
+left と rightどちらのテーブルを優先テーブルとして指定するかの違いがある。優先テーブルの情報は全て表示しつつ、他のテーブル情報に対になるデータがあれば表示し、ない場合はNULLで表示される仕組み。
+今回は結合条件にnullを指定したかった点（内部結合は結合条件にnullがある場合結合できない）、leftで指定したpeopleテーブルのnameを全件取得したかった点からleft outer joinを使用した。
